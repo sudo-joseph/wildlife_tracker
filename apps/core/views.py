c5738ed841334048015119230d00e605e64b907b
Masaaki Yana <nativeyana@gmail.com>
2019-12-05 22:38:31 -0800

    views.py: added IncidentReport form to use in add_new()

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django import forms 
 
-# Two example views. Change or delete as necessary.
+
+class IncidentReport(forms.Form):
+    reporter = forms.CharField(max_length=20)
+    email = forms.CharField(max_length=20)
+    phone = forms.CharField(max_length=20)
+
+    location_address = forms.CharField(max_length=120)
+    date_incident = forms.CharField(max_length=20)
+    date_reported = forms.CharField(max_length=20)
+    date_update = forms.CharField(max_length=20)
+    description = forms.CharField(max_length=20)
+
+
 def home(request):
 
     context = {
