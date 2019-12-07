7d681048b901110125e6fa7e09f87686ffc2e8b3
Joseph Reid <joseph.james.reid@gmail.com>
2019-12-07 00:12:53 -0800

    Test view functions with working lat lon retreival from website.

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import json
 # Two example views. Change or delete as necessary.
 def home(request):
 
@@ -17,8 +17,50 @@ def about(request):
 
 
 def map(request):
-    context = {
+    reports = [{'lat': '37.844170',
+                'lon': '-122.275153',
+                'sometext': 'number 1',
+                },
+               {'lat': '37.871459',
+                'lon': '-122.242401',
+                'sometext': 'number 2',
+                },
+               {'lat': '37.835747',
+                'lon': '-122.237114',
+                'sometext': 'number 3',
+                },
+               {'lat': '37.831025',
+                'lon': '-122.295640',
+                'sometext': 'number 4',
+                },
+               ]
+    context = {'reports': reports
+               }
 
+    return render(request, 'pages/map.html', context)
 
-    }
+def report(request):
+
+    reports = [{'lat': '37.844170',
+                'lon': '-122.275153',
+                'sometext': 'number 1',
+                },
+               {'lat': '37.871459',
+                'lon': '-122.242401',
+                'sometext': 'number 2',
+                },
+               {'lat': '37.835747',
+                'lon': '-122.237114',
+                'sometext': 'number 3',
+                },
+               {'lat': '37.831025',
+                'lon': '-122.295640',
+                'sometext': 'number 4',
+                },
+               ]
+    context = {'reports': reports
+               }
+    if request.method == 'POST':
+        print(json.loads(request.body))
+
     return render(request, 'pages/map.html', context)
