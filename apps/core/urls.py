b09f3befd8db9fa2439c5d9b97e2243f1d7a7172
Joseph Reid <joseph.james.reid@gmail.com>
2019-12-07 00:12:15 -0800

    Testing URLS for /map and /report. Need to replace with new code

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,5 +5,6 @@ from apps.core import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('map/', views.map, name='map')
+    path('map/', views.map, name='map'),
+    path('report/', views.report, name="map")
 ]
