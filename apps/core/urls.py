5d299a07ad32d5f6cb3eb47a969db1441b182e1f
Joseph Reid <joseph.james.reid@gmail.com>
2019-12-10 20:53:57 -0800

    Merge branch 'front-end' into location_fixes

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from apps.core import views
 
+app_name = 'core'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
