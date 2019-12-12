bca32e1776ca305870f30286c48991678136b5af
Masaaki Yana <nativeyana@gmail.com>
2019-12-12 12:19:58 -0800

    base.py: added MESSAGE_TAGS to meet error message with bootstrap alert class

--- wildlife_tracker/config/base.py
+++ wildlife_tracker/config/base.py
@@ -127,3 +127,10 @@ AUTH_USER_MODEL = 'accounts.User'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+from django.contrib.messages import constants as message_constants
+MESSAGE_TAGS = {message_constants.DEBUG: 'debug',
+                message_constants.INFO: 'info',
+                message_constants.SUCCESS: 'success',
+                message_constants.WARNING: 'warning',
+                message_constants.ERROR: 'danger',}
